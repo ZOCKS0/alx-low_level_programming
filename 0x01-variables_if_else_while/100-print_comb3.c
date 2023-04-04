1332efdf3c4442a7a88c2b6e23818c431ff7cb50
ZOCKS0 <zelkhamary1@gmail.com>
2023-04-03 20:05:03 -0700

    2c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,11 +6,14 @@
  * main - Entry point
  * Return : Always 0 (Success)
  */
+
 int main(void)
 {
 	int d, p;
+
 	for (d = '0'; d < '9'; d++)
 	{
+
 	for (p = d + 1; p <= '9'; p++)
 	{
 	if (p != d)
